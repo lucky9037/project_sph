9a50963a4783312a93500e1515a36f9b24397022
lucky <2680523347@qq.com>
2022-12-30 15:19:13 +0800

    提交了axios晚上接口测试

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <Header></Header>
     <!-- 路由出口 -->
+<!--    提交了axios晚上接口测试-->
     <router-view></router-view>
     <Footer v-show="this.$route.meta.show"></Footer>
   </div>
